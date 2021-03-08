234f67fe31e8e6604e37454bbc73112e58114269
Marius Alonso <marius.alonso@mines-paristech.fr>
2021-03-08 20:19:51 +0100

    test simulation random non fonctionnel

--- simulation.py
+++ simulation.py
@@ -1,27 +1,66 @@
 
+import bisect
+import random
 
 class Simulation():
-    def __init__(self, t0, stock, nb_robots):
+    def __init__(self, t0, stock, nb_robots, parking, occupation):
+        self.stock = stock
         self.nb_robots = nb_robots
+        self.t = t0
+        self.parking = parking
+        self.occupation = occupation
         self.events = []
         for v in stock.vehicles.values():
-            self.events.append(Event(v, v.order_deposit, "d"))
-            self.events.append(Event(v, v.order_retrieval, "r"))
+            self.events.append(Event(v, v.order_deposit, True))
+            self.events.append(Event(v, v.order_retrieval, False))
         self.events.sort()
+    
+    def next_event(self):
+        self.t = max(self.t, self.events[0].date)
+        for event in self.events:
+            if event.date <= self.t:
+                pass
+        algorithm = AlgorithmRandom(self.t, self.stock, self.nb_robots, self.parking, self.occupation)
+        algorithm.solve()
+
+
 
 class Event():
 
-    def __init__(self, vehicle, date, typ):
+    def __init__(self, vehicle, date, is_deposit):
         self.vehicle = vehicle
         self.date = date
-        self.typ = typ
+        self.is_deposit = is_deposit
+    
+    def __eq__(self, other):
+        return self.date == other.date
     
     def __lt__(self, other):
         return self.date < other.date
 
+
 class Algorithm():
-    def __init__(self):
-        pass
+    def __init__(self, t0, stock, nb_robots, parking, occupation):
+        self.nb_robots = nb_robots
+        self.stock = stock
+        self.t0 = t0
+        self.parking = parking
+        self.occupation = occupation
+
+
+class AlgorithmRandom(Algorithm):
+    
+    def solve(self):
+        for v in self.stock.vehicles.values():
+            if v.deposit <= self.t0:
+                while True :
+                    rand_block = random.choice(self.parking.blocks)
+                    rand_lane = random.choice(rand_block.lanes)
+                    #if random.randrange(2):
+                    if True : #rand_lane.top_position >= v.length :
+                        rand_lane.push_top(v.id)
+                        break
+                
 
 class Vehicle():
     def __init__(self, id_vehicle, deposit, retrieval, order_deposit, order_retrieval):
@@ -33,7 +72,7 @@ class Vehicle():
 
 
 class Stock():
-    def __init__(self, vehicles):
+    def __init__(self, *vehicles):
         self.vehicles = {}
         for v in vehicles:
             self.vehicles[v.id] = v
