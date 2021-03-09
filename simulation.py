4a5e95491b9ff8bbdec8608b19b446df30f8b826
Marius Alonso <marius.alonso@mines-paristech.fr>
2021-03-09 20:06:29 +0100

    modification de l'emplacement de occupation

--- simulation.py
+++ simulation.py
@@ -3,12 +3,15 @@ import bisect
 import random
 
 class Simulation():
-    def __init__(self, t0, stock, nb_robots, parking, occupation):
+    def __init__(self, t0, stock, nb_robots, parking):
         self.stock = stock
+        for i, event in enumerate(self.stock.order_events) :
+            if event.date >= t0 :
+                break
+        self.i_order_events = i
         self.nb_robots = nb_robots
         self.t = t0
         self.parking = parking
-        self.occupation = occupation
     
     def next_event(self):
         """
@@ -17,7 +20,7 @@ class Simulation():
             if event.date <= self.t:
                 pass
         """
-        algorithm = AlgorithmRandom(self.t, self.stock, self.nb_robots, self.parking, self.occupation)
+        algorithm = AlgorithmRandom(self.t, self.stock, self.nb_robots, self.parking)
         algorithm.solve()
 
 
@@ -37,12 +40,11 @@ class Event():
 
 
 class Algorithm():
-    def __init__(self, t0, stock, nb_robots, parking, occupation):
+    def __init__(self, t0, stock, nb_robots, parking):
         self.nb_robots = nb_robots
         self.stock = stock
         self.t0 = t0
         self.parking = parking
-        self.occupation = occupation
     
     def simple_pick_up(self, vehicle):
         pass
@@ -54,13 +56,20 @@ class AlgorithmRandom(Algorithm):
         for v in self.stock.vehicles.values():
             if v.deposit <= self.t0:
                 while True :
-                    rand_block = random.choice(self.parking.blocks)
-                    rand_lane = random.choice(rand_block.lanes)
-                    #if random.randrange(2):
-                    if True : #rand_lane.top_position >= v.length :
-                        rand_lane.push_top(v.id)
-                        break
-        
+                    rand_i_block = random.randrange(len(self.parking.blocks))
+                    rand_i_lane = random.randrange(len(self.parking.blocks[rand_i_block].lanes))
+                    lane_chosen = self.parking.blocks[rand_i_block].lanes[rand_i_lane]
+                    if random.randrange(2):
+                        if True : #rand_lane.top_position >= v.length :
+                            lane_chosen.push_top(v.id)
+                            self.parking.occupation[v.id] = (rand_i_block, rand_i_lane, lane_chosen.top_position)
+                            break
+                    else:
+                        if True : #lane_chosen.top_position >= v.length :
+                            lane_chosen.push_bottom(v.id)
+                            self.parking.occupation[v.id] = (rand_i_block, rand_i_lane, lane_chosen.bottom_position)
+                            break
+                                               
                 
 
 class Vehicle():
--- test_simulation.py
+++ test_simulation.py
@@ -10,6 +10,6 @@ class TestTest() :
     def test_random(self) :
         stock = Stock([Vehicle(1, 0, 1, -1, -1), Vehicle(2, 0, 2, -1, -1), Vehicle(3, 0, 3, -1, -1)])
         parking = Parking([Block([Lane(1, None, 10, 10, 10), Lane(2, None, 10, 10, 10)])])
-        simulation = Simulation(0, stock, 1, parking, {})
+        simulation = Simulation(0, stock, 1, parking)
         simulation.next_event()
         assert 1 in simulation.parking.blocks[0].lanes[0].dict_vehicle or 1 in simulation.parking.blocks[0].lanes[1].dict_vehicle
