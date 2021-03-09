d33183dcdaeb9aa3728a57a7751eebec91dfd7b5
Enol Alvarez <enol.alvarez_rodriguez@mines-paristech.fr>
2021-03-09 19:07:36 +0100

    passage au matriciel

--- parking.py
+++ parking.py
@@ -1,4 +1,5 @@
 from simulation import Vehicle
+import numpy as np
 
 class Parking() :
     def __init__(self, blocks):
@@ -9,28 +10,34 @@ class Block() :
     def __init__(self, lanes):
         self.lanes = lanes
 
-
 class Lane() :
-    def __init__(self, id_lane, block, length, top_position, bottom_position, top_access = True, bottom_access = True) :
+    def __init__(self, id_lane, length, top_access = True, bottom_access = True):
         self.length = length
         self.id = id_lane
-        self.block = block
+        self.list_vehicles = np.empty(self.length)
         self.length = length
-        self.top_position = top_position
-        self.bottom_position = bottom_position
+        self.top_position = None
+        self.bottom_position = None
         self.top_access = top_access
         self.bottom_access = bottom_access
-        self.dict_vehicle = dict()                           # (id_vehicle : [id_vehicle_top, id_vehicle_bottom])
         self.top_id_vehicle = None
         self.bottom_id_vehicle = None
 
-    def push_top(self, id_vehicle): 
-        if not self.dict_vehicle:
-            self.dict_vehicle[id_vehicle] = [None, None]
-            self.top_id_vehicle = id_vehicle
-            self.bottom_id_vehicle = id_vehicle
+    def push_top(self, id_vehicle):
+        if self.top_position == None:
+            if not bottom_access:
+                self.list_vehicles[self.length - 1] = id_vehicle
+                self.top_position = self.length - 1
+                self.bottom_position = self.length -1
+            else:
+                self.list_vehicles[self.length//2] = id_vehicle
+                self.top_position = self.length//2
+                self.bottom_position = self.length//2
         else:
-            self.dict_vehicle[self.top_id_vehicle][0] = id_vehicle   #on ajoute le nouveau véhicule au top de l'ancien top véhicule
-            self.dict_vehicle[id_vehicle] = [None, self.top_id_vehicle]
-            self.top_id_vehicle = id_vehicle
+            self.list_vehicles[self.top_position-1] = id_vehicle
+            self.top_position -= 1
 
+
+       
+
+    
--- test.py
+++ test.py
@@ -6,20 +6,10 @@ class TestTest() :
         #assert Parking(Block(...)) == ...
         pass
 
-    def test_push_lane(self):
+    def test_push_top():
         vehicle_1 = Vehicle(1,1,2,3,4)
-        vehicle_2 = Vehicle(2,12,22,32,42)
-        block = Block(5)
-        lane = Lane(1, block, 10, 2, 5)
+        lane = Lane(0, 10)
         lane.push_top(vehicle_1.id) 
-        assert lane.dict_vehicle == {vehicle_1.id: [None, None]}
+        assert lane.list_vehicles == []
 
-    def test_push_lane_2(self):
-        vehicle_1 = Vehicle(1,1,2,3,4)
-        vehicle_2 = Vehicle(2,12,22,32,42)
-        block = Block(5)
-        lane = Lane(1, block, 10, 2, 5)
-        lane.push_top(vehicle_1.id) 
-        lane.push_top(vehicle_2.id)
-        assert lane.dict_vehicle == {vehicle_1.id: [vehicle_2.id, None], vehicle_2.id: [None, vehicle_1.id]}
 
