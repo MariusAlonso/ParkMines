781aec1dc7b01eb0a1c1b108c9350398f23f7369
louisotpierre <louisot.pierre@gmail.com>
2021-03-10 19:14:09 +0100

    inputs avec mocker et tests fonctionnels

--- inputs.py
+++ inputs.py
@@ -1,36 +1,62 @@
-import datetime as dt
-import re
+import datetime
+from copy import deepcopy
+import argparse
+from vehicle import Vehicle
+import sys
 
-movements_list = []
-first_line = True
+path = "inputs/movements.csv"
 
-with open("inputs/movements.csv", "r") as movements:
-    for line in movements:
+#### récupération du path ####
 
-        #non-traitement de l'en-tête
-        if first_line:
-            first_line = False
-        else:
+def get_path():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--path", nargs='?', default="inputs/movements.csv", help="path of the file to load", type=str)
+    print(sys.argv)
+    args = parser.parse_args()
+    return args.path
 
-            movement = line.split(",")
-            #reformatage des données véhicule
-            deposit = movement[0]
-            retrieval = movement[1]
+#### import des véhicules à partir d'un fichier texte ####
 
-            delimiters = [':', 'T', 'Z']
-            for delimiter in delimiters:
-                deposit = deposit.replace(delimiter, '-')
-                retrieval = retrieval.replace(delimiter, '-')
-            
-            deposit = deposit.split('-')[:-1]
-            retrieval = retrieval.split('-')[:-1]
+def import_from_file():
 
-            deposit = dt.datetime(*(int(item) for item in deposit))
-            retrieval = dt.datetime(*(int(item) for item in retrieval))
-            
-            booking_id = movement[2].strip()
+    movements_list = []
+    #path = get_path()
 
-            movements_list.append([deposit, retrieval, booking_id])
+    with open(path, "r") as movements:
+        first_line = True
 
-for item in movements_list[0]:
-    print(item)
+        for line in movements:
+
+            #non-traitement de l'en-tête
+            if first_line:
+                first_line = False
+            else:
+
+                movement = line.split(",")
+                #reformatage des données véhicule
+
+                deposit = movement[0]
+                retrieval = movement[1]
+                #booking_id non utilisé dans la suite
+                booking_id = movement[2].strip()
+
+                #reformatage des dates
+                delimiters = [':', 'T', 'Z']
+                for delimiter in delimiters:
+                    deposit = deposit.replace(delimiter, '-')
+                    retrieval = retrieval.replace(delimiter, '-')
+                
+                deposit = deposit.split('-')[:-1]
+                retrieval = retrieval.split('-')[:-1]
+
+                #conversion au format datetime
+                deposit = datetime.datetime(*(int(item) for item in deposit))
+                retrieval = datetime.datetime(*(int(item) for item in retrieval))
+                order_deposit = deepcopy(deposit)
+                order_retrieval = deepcopy(retrieval)
+
+                movements_list.append(Vehicle(1, deposit, retrieval, order_deposit, order_retrieval))
+
+    return movements_list
+
+print(import_from_file()[0].deposit)
