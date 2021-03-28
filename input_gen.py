51cc62074895d14e2962ef9b18f48f180da89004
Marius Alonso <marius.alonso@mines-paristech.fr>
2021-03-29 00:39:18 +0200

    feat : graphiques sommaires sur les KPI

---
+++ input_gen.py
@@ -0,0 +1,28 @@
+
+import numpy as np
+import datetime
+import random
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+from sklearn.utils import shuffle
+
+mu_deposit_order = datetim
+sigma_deposit_order = 12
+
+mu_retrieval_order = 
+sigma_retrieval_order = 
+
+mu_stay_duration = 
+sigma_stay_duration = 
+
+mu_entrances = 
+sigma_entrances = 
+
+mu_exits = 
+sigma_exits = 
+
+def generate(flux_density=1, time=datetime.timedelta(days=31), start_date=datetime.date(2021, 1, 1)):
+
+    movements = pd.DataFrame(columns = ["ORDER_DEPOSIT", "DEPOSIT", "ORDER_RETRIEVAL", "RETRIEVAL"])
+
