ef8b0af8c1d5dd837c334dc439addbf847c4dd97
Enol Alvarez <enol.alvarez_rodriguez@mines-paristech.fr>
2021-04-27 20:56:55 +0200

    cd

--- test_display.py
+++ test_display.py
@@ -9,7 +9,7 @@ class TestTest():
     def test_display_parking(self):
         
         Vehicle.next_id = 1
-        stock = RandomStock(30, time = datetime.timedelta(days=1000))
+        stock = RandomStock(25, time = datetime.timedelta(days=100))
         print(len(stock.vehicles))
         parking = Parking([BlockInterface([Lane(1, 1), Lane(2, 1), Lane(3, 1)]), Block([], 15, 10), Block([Lane(1, 4), Lane(2, 4)]), Block([],6,3)], [[0,0,0,0],["s",1,1,1],[2,2,3,"e"]])
         real_parking2 = Parking([BlockInterface([],10,1,"leftrigth"), Block([], 15, 7), Block([], 14, 7), Block([], 13, 6), Block([], 8, 7), Block([], 18, 7), Block([], 10, 11,"leftrigth")], [['s', 'f7:0',1,1,1],['s', 'f3:0',1,1,1], ['s','f3:0',0,2,2], ['s','f3:0',3,3,3], ['s',5,5,4,4], ['s','f3:0',6,6,6]])
@@ -17,7 +17,7 @@ class TestTest():
         real_parking = Parking([BlockInterface([],10,1), Block([], 15, 7,"leftrigth"), Block([], 14, 7,"leftrigth"), Block([], 13, 6,"leftrigth"), Block([], 8, 7,"leftrigth"), Block([], 18, 7,"leftrigth"), Block([], 10, 11), Block([], 15, 1, "leftrigth")], [['s','s', 'f0:6', 'f0:6', 'e', 4, 6], [7,1,1,2,'f0:3', 4,6], [7,1,1,2,3,'f0:2', 6], [7,1,1,2,3,5,6], [7,'e','e','e',3,5,6], [7,'e','e','e','e',5,6], [7,'f7:0',0,0,0,5,6]])
         with open("log.txt", "w") as log_file:
             sys.stdout = log_file
-            display = Display(datetime.datetime(2016,1,1,0,0,0,0), stock, [Robot(1), Robot(2), Robot(3), Robot(4)], real_parking, AlgorithmRandom, 12, 20, print_in_terminal = True)
+            display = Display(datetime.datetime(2016,1,1,0,0,0,0), stock, [Robot(1), Robot(2), Robot(3), Robot(4)], real_parking, AlgorithmRandom, 12, 20, print_in_terminal = False)
 
         assert 0 == 0
 
