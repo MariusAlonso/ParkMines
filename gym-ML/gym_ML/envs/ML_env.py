6835ecb81a6b343b990922ac85666afdbde117f6
Enol Alvarez <enol.alvarez_rodriguez@mines-paristech.fr>
2021-04-29 13:54:24 +0200

    commit

--- gym-ML/gym_ML/envs/ML_env.py
+++ gym-ML/gym_ML/envs/ML_env.py
@@ -6,10 +6,10 @@ class MLEnv(gym.Env):
   metadata = {'render.modes': ['human']}
 
   def __init__(self):
-    ...
+    pass
   def step(self, action):
-    ...
+    pass
   def reset(self):
-    ...
+    pass
   def render(self, mode='human', close=False):
-    ...
+    pass
